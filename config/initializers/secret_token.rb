a9c49927ac7a47e9772718f0f988f074293eb6b7
Seth Allen <seth.a@me.com>
2015-09-27 21:36:19 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flowalertsystems::Application.config.secret_key_base = '291e72c28ea17385fb4c5f46e6311af19c89223c0fa00e2e8e0702f86ee51541fe0a3656d1cdf7b57f188e2c7ce1297ecf7759e66976da904a773769f3607b66'
